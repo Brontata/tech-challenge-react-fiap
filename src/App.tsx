5f4d9feff21ba66f4df5ee18e2ad902257a24e76
Eusébio Souza <eusebiops14@gmail.com>
2024-09-12 19:41:35 -0300

    feat(devop): criando listagem basica dos posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,35 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Aside from "./components/Aside";
 import Posts from "./pages/Posts";
+import PageNewPost from "./pages/PageNewPost";
+import styled from "styled-components";
+
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Posts />,
   },
+  {
+    path: "/newPost",
+    element: <PageNewPost />
+  }
 ]);
+
+const StyledBody = styled.div`
+  background-color: #90e0ef;
+`;
 
 function App() {
   return (
-    <>
-      <Aside />
-      <div className="content-wrapper px-4 py-2">
-        <RouterProvider router={routes} />
-      </div>
-    </>
+    <StyledBody>
+      <>
+        <Aside />
+        <div className="content-wrapper px-4 py-2">
+          <RouterProvider router={routes} />
+        </div>
+      </>
+    </StyledBody>
   );
 }
 
--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+//import React from "react";
+import Card from "../../components/card";
+import Post from "../../types/Post";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const postsExample: Post[] = [
+
+    {
+      id: 1,
+      user_id: 1,
+      title: 'Equacao Primeiro Grau',
+      description: 'Para calcular as raizes de uma equação do primeiro grau deve-se informar o valor de A e B',
+      slug: 'slug',
+      //created_at: new Date()
+    },
+
+    {
+      id: 2,
+      user_id: 2,
+      title: 'Equacao Segundo Grau',
+      description: 'Para calcular as raizes de uma equação do segundo grau deve-se informar o valor de A, B e C',
+      slug: 'slug2',
+      //created_at: new Date()
+    },
+
+    {
+      id: 3,
+      user_id: 1,
+      title: 'Area de um Circulo',
+      description: 'Para calcular a area de um circulo deve-se informar o raio do circulo e o valor do raio deve ser informado em cm',
+      slug: 'o que era mesmo ?',
+      //created_at: new Date()
+
+    }
+
+  ]
+
+
+  //   
+
+  return (
+    <>
+      <h2>Últimos Posts</h2>
+      {postsExample.map((post) => (
+        <Card
+          key={post.id}
+          id={post.id}
+          user_id={post.user_id}
+          title={post.title}
+          description={post.description}
+          slug={post.slug}
+        />
+      ))}
+
+    </>
+  )
 };
 
 export default Posts;
